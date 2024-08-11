e3206118ac579e459b1b18b91657f3b69a16365d
Bugee03 <440098@ogr.ktu.edu.tr>
2024-08-11 16:24:22 +0300

    11.08.24

---
+++ docNvim/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+  
+  //here for comment 
+  
+  int doc = 12;
+  int new = 13;
+  printf("%d\n",doc + new);
+
+  printf("New chapter of my life !!!\n");
+  return 0;
+
+}
